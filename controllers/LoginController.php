4fc8433e99dff0d0f87cbc879eac20c9d768f7e6
NathalieMassiel <nathalie.arroyaves@gmail.com>
2023-11-27 15:03:49 -0500

    Login

---
+++ controllers/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Controllers;
+
+class LoginController {
+    public static function login() 
+    {
+        echo"Desde login";
+    }
+
+    public static function logout() 
+    {
+        echo"Desde logout";
+    }
+
+    public static function olvide() 
+    {
+        echo "Desde olvide";
+    }
+    
+    public static function recuperar() 
+    {
+        echo "Desde recuperar";
+    }
+}
+
