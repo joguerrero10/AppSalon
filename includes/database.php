69262ee2188b3600e6122470db466be3437addd8
joguerrero10 <joelg1014@hotmail.com>
2023-11-28 11:54:10 +0500

    test

--- includes/database.php
+++ includes/database.php
@@ -1,21 +1,13 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databasename = "blackninja";
+$port = 3306;
 
-class Conexion
-{
-
-  static public function conectar()
-  {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $databasename = "blackninja";
-    $port = 3306;
-
-    try {
-      $conn = new PDO("mysql:host=$servername;dbname=$databasename;port=$port", $username, $password);
-      return $conn;
-    } catch (PDOException $e) {
-      echo "Connection failed: " . $e->getMessage();
-    }
-  }
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$databasename;port=$port", $username, $password);
+    return $conn;
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
