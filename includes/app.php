131973abdbb304b2b58357da0c077c593eaedbec
joguerrero10 <joelg1014@hotmail.com>
2023-11-28 11:37:07 +0500

    conexiones

--- includes/app.php
+++ includes/app.php
@@ -1,9 +1,12 @@
 <?php 
-
-require 'funciones.php';
 require 'database.php';
+require 'funciones.php';
+
 require __DIR__ . '/../vendor/autoload.php';
+
 
 // Conectarnos a la base de datos
 use Model\ActiveRecord;
 ActiveRecord::setDB($db);
+
+
--- includes/database.php
+++ includes/database.php
@@ -1,14 +1,13 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = ""; // Si la contraseña está vacía, déjala así
+$password = "";
+$databasename = "blackninja";
+$port = 3306;
 
 try {
-    $conn = new PDO("mysql:host=$servername", $username, $password);
-    // Establecer el modo de error de PDO en excepción
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa";
-} catch(PDOException $e) {
-    echo "Error en la conexión: " . $e->getMessage();
+    $db = new PDO("mysql:host=$servername;dbname=$databasename;port=$port", $username, $password);
+    return $db;
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
-?>
