d32984d292e596b0238bc87e6ed6b7f292a7ee43
joguerrero10 <148748354+joguerrero10@users.noreply.github.com>
2023-11-30 06:44:00 +0500

    Basesdelproyecto2 (#9)
    * Login
    
    * Cambie la conexión
    
    * Update package.json
    
    * conexiones
    
    * solucion
    
    * test
    
    ---------
    
    Co-authored-by: NathalieMassiel <nathalie.arroyaves@gmail.com>
    Co-authored-by: diegoagch24 <diegoagch24@gmail.com>
    Co-authored-by: NathalieMassiel <77088418+NathalieMassiel@users.noreply.github.com>

---
+++ includes/app.php
@@ -0,0 +1,13 @@
+<?php 
+require 'database.php';
+
+require 'funciones.php';
+
+require __DIR__ . '/../vendor/autoload.php';
+
+
+// Conectarnos a la base de datos
+// use Model\ActiveRecord;
+// ActiveRecord::setDB($db);
+
+
