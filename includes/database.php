932e3afcc78263ea16144f0aff15b4a1bd1ea873
joguerrero10 <148748354+joguerrero10@users.noreply.github.com>
2023-11-30 06:44:32 +0500

    Develop (#10)
    * test2
    
    * mensaje
    
    * Basesdelproyecto2 (#9)
    
    * Login
    
    * Cambie la conexión
    
    * Update package.json
    
    * conexiones
    
    * solucion
    
    * test
    
    ---------
    
    Co-authored-by: NathalieMassiel <nathalie.arroyaves@gmail.com>
    Co-authored-by: diegoagch24 <diegoagch24@gmail.com>
    Co-authored-by: NathalieMassiel <77088418+NathalieMassiel@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: NathalieMassiel <nathalie.arroyaves@gmail.com>
    Co-authored-by: NathalieMassiel <77088418+NathalieMassiel@users.noreply.github.com>
    Co-authored-by: diegoagch24 <diegoagch24@gmail.com>
    Co-authored-by: diegoagch24 <152032747+diegoagch24@users.noreply.github.com>
    Co-authored-by: diegoagch24 <diegoagch24@gmail.com>

---
+++ includes/database.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$databasename = "blackninja";
+$port = 3306;
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$databasename;port=$port", $username, $password);
+    return $conn;
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
